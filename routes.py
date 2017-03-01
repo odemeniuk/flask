6b8e31fd31a1c48ed9e84b0a9c532d060ddc1179
Oleksandr Demeniuk <demeniuk@gmail.com>
2017-03-01 23:41:48 +0200

    added lib

--- routes.py
+++ routes.py
@@ -4,7 +4,7 @@ from forms import SignupForm
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/learningflask'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/Air1'
 db.init_app(app)
 
 app.secret_key = "development-key"
